dedde1df9a0b055896eae85ef810335781402a31
Renat <rent1995@ya.ru>
2022-12-27 12:43:50 +0700

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,101 +1,78 @@
-module.exports = function toReadable (num) {
+module.exports = function toReadable (number) {
     
+    let num = {
+        1: 'one',
+        2: 'twen',
+        3: 'thir',
+        4: 'for',
+        5: 'fif',
+        6: 'six',
+        7: 'seven',
+        8: 'eigh',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+    }
 
-        let words = ['one', 'twen', 'thir', 'for', 'fif', 'six', 'seven', 'eigh', 'nine', 'ten', 'eleven', 'twelve', 'teen', 'ty', 'hundred']
-        let singleNum = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-        let result = '';
-        
-        if (num.toString().length === 3) {
-    
-            for (let i = 0; i < 10; i++) {
-    
-                if (+num.toString()[0] === i) {
-                    result = words[i-1] + ' ' + words[14]
-                }
-    
-            }
-    
-            if (+num.toString()[1] === 1 && +num.toString()[2] === 1) {
-                result = result + ' ' + words[10]
-            } else if (+num.toString()[1] === 1 && +num.toString()[2] === 2) {
-                result = result + ' ' + words[11]
-            } else if (+num.toString()[1] === 1 && +num.toString()[2] === 0) {
-                result = result + ' ' + words[9]
-            } else
-             if (+num.toString()[0] >= 2 ) {
-                for (let i = 0; i < 10; i++) {
-    
-                    if (+num.toString()[0] === i) {
-                        result = result + ' ' + words[i-1] + words[13]
-                    }
-    
-                }
-    
-                for (let i = 0; i < 10; i++) {
-                    if (+num.toString()[1] === 0) {
-                        continue
-                    } else if (+num.toString()[1] === i) {
-                        result = result + ' ' + singleNum[i-1]
-                    }
-    
-                }
-            } else {
-                for (let i = 0; i < 10; i++) {
-    
-                    if (+num.toString()[1] === i) {
-                        result = words[i-1] + words[12]
-                    }
-    
-                }
-            }
-    
-    
-        } else if (num.toString().length === 2) {
-    
-            if (+num.toString()[0] === 1 && +num.toString()[1] === 1) {
-                result = words[10]
-            } else if (+num.toString()[0] === 1 && +num.toString()[1] === 2) {
-                result = words[11]
-            } else if (+num.toString()[0] === 1 && +num.toString()[1] === 0) {
-                result = words[9]
-            } else
-             if (+num.toString()[0] >= 2 ) {
-                for (let i = 0; i < 10; i++) {
-    
-                    if (+num.toString()[0] === i) {
-                        result = words[i-1] + words[13]
-                    }
-    
-                }
-    
-                for (let i = 0; i < 10; i++) {
-                    if (+num.toString()[1] === 0) {
-                        continue
-                    } else if (+num.toString()[1] === i) {
-                        result = result + ' ' + singleNum[i-1]
-                    }
-    
-                }
-            } else {
-                for (let i = 0; i < 10; i++) {
-    
-                    if (+num.toString()[1] === i) {
-                        result = words[i-1] + words[12]
-                    }
-    
-                }
-            }
-    
-        } else {
-            for (let i = 0; i < 10; i++) {
-    
-                if (+num.toString()[0] === i) {
-                    result = singleNum[i-1]
-                }
-    
-            }
-        }
-    
+    let fullNumber = {
+        0: 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+    }
+
+    let result = '';
+    let firstNumber = number % 10 ;
+    let secondNumber = Math.floor( number % 100 / 10);
+    let thirdNumber = Math.floor( number % 1000 / 100);
+
+//  3х знач числа
+    if(+number.toString().length === 3 && secondNumber === 0 && firstNumber === 0){
+        result = fullNumber[thirdNumber] + ' hundred'
+    } else if(+number.toString().length === 3 && secondNumber === 1 && firstNumber === 0) {
+        result = fullNumber[thirdNumber] + ' hundred ' + fullNumber[10]
+    } else if(+number.toString().length === 3 && secondNumber === 1 && firstNumber === 1) {
+        result = fullNumber[thirdNumber] + ' hundred ' + fullNumber[11]
+    } else if(+number.toString().length === 3 && secondNumber === 1 && firstNumber === 2) {
+        result = fullNumber[thirdNumber] + ' hundred ' + fullNumber[12]
+    } else if(+number.toString().length === 3 && secondNumber === 1 && firstNumber === 4) {
+        result = fullNumber[thirdNumber] + ' hundred ' + fullNumber[firstNumber] + 'teen'
+    } else if(+number.toString().length === 3 && secondNumber === 1) {
+        result = fullNumber[thirdNumber] + ' hundred ' + num[firstNumber] + 'teen'
+    } else if(+number.toString().length === 3 && firstNumber === 0) {
+        result = fullNumber[thirdNumber] + ' hundred ' + num[secondNumber] + 'ty'
+    } else if (+number.toString().length === 3 && secondNumber === 0) {
+        result = fullNumber[thirdNumber] + ' hundred ' + fullNumber[firstNumber]
+    }else if(+number.toString().length === 3) {
+        result = fullNumber[thirdNumber] +  ' hundred '  + num[secondNumber] + 'ty ' + fullNumber[firstNumber]
+    } else if(+number.toString().length === 2 && secondNumber === 1 && firstNumber === 0) {
+        result = fullNumber[10]
+    } else if(+number.toString().length === 2 && secondNumber === 1 && firstNumber === 1) {
+        result = fullNumber[11]
+    } else if(+number.toString().length === 2 && secondNumber === 1 && firstNumber === 2) {
+        result = fullNumber[12]
+    } else if(+number.toString().length === 2 && secondNumber === 1 && firstNumber === 4) {
+        result = fullNumber[firstNumber] + 'teen'
+    } else if(+number.toString().length === 2 && secondNumber === 1) {
+        result = num[firstNumber] + 'teen'
+    } else if(+number.toString().length === 2 && firstNumber === 0) {
+        result = num[secondNumber] + 'ty'
+    } else if(+number.toString().length === 2) {
+        result = num[secondNumber] +  'ty ' + fullNumber[firstNumber]
+    } if (+number.toString().length === 1) {
+        result = fullNumber[firstNumber]
+    }
+    return result
         
     }
     
